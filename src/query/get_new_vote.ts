2bcf25ad09b4bef950eb7f5e28a9a35ab6b5eb1a
Alexdelia <alexandre.delille.57@gmail.com>
2025-10-13 19:29:42 +0200

    feat: progress on dog query

--- src/query/get_new_vote.ts
+++ src/query/get_new_vote.ts
@@ -1,3 +1,5 @@
+import { randomBetween } from "@std/random"
+import getBreedFromImageUrl from "./get_breed.ts"
 import { supabase } from "./index.ts"
 
 const dogApiUrl = "https://dog.ceo/api/breeds/image/random"
@@ -15,15 +17,36 @@ export default async function getNewVote(): Promise<string[]> {
 		tries++
 
 		const lastDog = dogData.at(0)
-		const fetchNew = !lastDog || topDogVoteCount === 0 ||
-			(lastDog.voteCount / topDogVoteCount) > 0.5 // FIXME: use random number generator
+		const fetchNew = !lastDog || topDogVoteCount === null ||
+			(topDogVoteCount > 0
+				? (lastDog.voteCount / topDogVoteCount) < randomBetween(0, 1)
+				: false)
 
-		if (fetchNew) {
-			const newDogImageUrl = await getNewDogFromApi()
-			if (!newDogImageUrl) {
+		if (!fetchNew) {
+			const randomDogImageUrl = await getRandomDogImageUrlFromDb(
+				res.map((d) => d.imageUrl),
+			)
+			if (randomDogImageUrl) {
+				res.push({ imageUrl: randomDogImageUrl, voteCount: 0 })
 				continue
 			}
 		}
+
+		const newDogImageUrl = await getNewDogFromApi()
+		if (
+			!newDogImageUrl ||
+			res.some((d) => d.imageUrl === newDogImageUrl)
+		) {
+			continue
+		}
+
+		const breed = getBreedFromImageUrl(newDogImageUrl)
+		await supabase.from("dog_history").upsert({
+			breed,
+			image_url: newDogImageUrl,
+		})
+
+		res.push({ imageUrl: newDogImageUrl, voteCount: 0 })
 	}
 
 	return res.length === 2 ? res.map((d) => d.imageUrl) : []
@@ -34,6 +57,7 @@ async function getLastTwoDog() {
 		.from("dog_history")
 		.select("voteCount:vote_count, imageUrl:image_url")
 		.order("vote_count", { ascending: true })
+		.order("updated_at", { ascending: true })
 		.limit(2)
 	if (error) {
 		// deno-lint-ignore no-console
@@ -43,7 +67,7 @@ async function getLastTwoDog() {
 	return dogData ?? []
 }
 
-async function getTopDogVoteCount(): Promise<number> {
+async function getTopDogVoteCount(): Promise<number | null> {
 	const { data, error } = await supabase
 		.from("dog_history")
 		.select("voteCount:vote_count")
@@ -52,9 +76,9 @@ async function getTopDogVoteCount(): Promise<number> {
 	if (error) {
 		// deno-lint-ignore no-console
 		console.error(error)
-		return 0
+		return null
 	}
-	return data?.[0]?.voteCount ?? 0
+	return data?.[0]?.voteCount ?? null
 }
 
 async function getNewDogFromApi(): Promise<string | null> {
@@ -75,4 +99,26 @@ async function getNewDogFromApi(): Promise<string | null> {
 		console.error(e)
 		return null
 	}
+}
+
+async function getRandomDogImageUrlFromDb(
+	excludeUrls: string[],
+): Promise<string | null> {
+	const { data, error } = await supabase
+		.from("dog_history")
+		.select("imageUrl:image_url")
+		.not(
+			"image_url",
+			"in",
+			`(${excludeUrls.map((u) => `'${u}'`).join(",")})`,
+		)
+		.order("RANDOM()")
+		.limit(1)
+		.single()
+	if (error) {
+		// deno-lint-ignore no-console
+		console.error(error)
+		return null
+	}
+	return data?.imageUrl ?? null
 }
