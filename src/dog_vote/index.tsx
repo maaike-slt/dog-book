4b5fcd918babdb3165255dc305d8cf3da080ab23
maaike-slt <maaikesloot@home.nl>
2025-10-18 17:36:28 +0200

    feat: some cool animation

--- src/dog_vote/index.tsx
+++ src/dog_vote/index.tsx
@@ -6,6 +6,7 @@ import "./index.css"
 
 const DogVote: React.FC = () => {
 	const [dogVote, setDogVote] = useState<string[]>([])
+	const [votedIndex, setVotedIndex] = useState<number | null>(null)
 
 	useEffect(() => {
 		handleGetNewVote()
@@ -23,12 +24,16 @@ const DogVote: React.FC = () => {
 		setDogVote(res)
 	}
 
-	async function handleCastVote(
-		upDogUrl: string,
-		downDogUrl: string,
-	): Promise<void> {
-		await castVote(upDogUrl, downDogUrl)
-		await handleGetNewVote()
+	function handleCastVote(
+		index: number,
+	): void {
+		setVotedIndex(index)
+		const castVoteProcess = castVote(dogVote[index], dogVote[1 - index])
+		setTimeout(async () => {
+			setVotedIndex(null)
+			await castVoteProcess
+			await handleGetNewVote()
+		}, 500)
 	}
 
 	if (dogVote.length !== 2) {
@@ -41,23 +46,33 @@ const DogVote: React.FC = () => {
 			<button
 				type="button"
 				className="dog-button"
-				onClick={() => handleCastVote(dogVote[0], dogVote[1])}
+				onClick={() => handleCastVote(0)}
 			>
 				<img
 					src={dogVote[0]}
 					alt="Dog 1"
-					className="dog-image"
+					className={`dog-image` +
+						(votedIndex === null
+							? ""
+							: (votedIndex === 0
+								? " dog-image-up-vote"
+								: " dog-image-down-vote"))}
 				/>
 			</button>
 			<button
 				type="button"
 				className="dog-button"
-				onClick={() => handleCastVote(dogVote[1], dogVote[0])}
+				onClick={() => handleCastVote(1)}
 			>
 				<img
 					src={dogVote[1]}
 					alt="Dog 2"
-					className="dog-image"
+					className={`dog-image` +
+						(votedIndex === null
+							? ""
+							: (votedIndex === 1
+								? " dog-image-up-vote"
+								: " dog-image-down-vote"))}
 				/>
 			</button>
 		</>
