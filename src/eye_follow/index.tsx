903cf1fef2ff8fbbbfaa27c977577d9b28e887e3
Alexdelia <alexandre.delille.57@gmail.com>
2025-10-18 10:26:53 +0200

    feat(eye-follow): compute eye center position `px`

--- src/eye_follow/index.tsx
+++ src/eye_follow/index.tsx
@@ -21,6 +21,17 @@ const EYE_BASE_STYLE = {
 	zIndex: BASE.zIndex + 1,
 } as const
 
+const EYE_POS_OFFSET = {
+	left: {
+		x: BASE.x + (217.5 / IMG_PX_SIZE * BASE.size),
+		y: BASE.y + (429.5 / IMG_PX_SIZE * BASE.size),
+	},
+	right: {
+		x: BASE.x + (358.5 / IMG_PX_SIZE * BASE.size),
+		y: BASE.y + (458.5 / IMG_PX_SIZE * BASE.size),
+	},
+}
+
 const STYLE = {
 	img: {
 		position: "fixed",
@@ -32,13 +43,13 @@ const STYLE = {
 	eye: {
 		left: {
 			...EYE_BASE_STYLE,
-			left: BASE.x + (217.5 / IMG_PX_SIZE * BASE.size) + BASE.unit,
-			bottom: BASE.y + (429.5 / IMG_PX_SIZE * BASE.size) + BASE.unit,
+			left: EYE_POS_OFFSET.left.x + BASE.unit,
+			bottom: EYE_POS_OFFSET.left.y + BASE.unit,
 		},
 		right: {
 			...EYE_BASE_STYLE,
-			left: BASE.x + (358.5 / IMG_PX_SIZE * BASE.size) + BASE.unit,
-			bottom: BASE.y + (458.5 / IMG_PX_SIZE * BASE.size) + BASE.unit,
+			left: EYE_POS_OFFSET.right.x + BASE.unit,
+			bottom: EYE_POS_OFFSET.right.y + BASE.unit,
 		},
 	},
 } as const
@@ -66,15 +77,29 @@ const Eyes: React.FC = () => {
 	const { x, y } = useMousePosition()
 	const w = globalThis.window.innerWidth
 	const h = globalThis.window.innerHeight
+	const eyeCenterPosPx = useMemo(() => {
+		return {
+			left: {
+				x: (EYE_POS_OFFSET.left.x + EYE_SIZE) * fontSize - 0.5,
+				y: h - ((EYE_POS_OFFSET.left.y + EYE_SIZE) * fontSize) - 0.5,
+			},
+			right: {
+				x: (EYE_POS_OFFSET.right.x + EYE_SIZE) * fontSize - 0.5,
+				y: h - ((EYE_POS_OFFSET.right.y + EYE_SIZE) * fontSize) - 0.5,
+			},
+		}
+	}, [fontSize, w, h])
 
 	console.debug({ x, y, fontSize, w, h })
 
 	return (
 		<>
 			<svg style={STYLE.eye.left}>
+				<title>left eye</title>
 				<circle cx="50%" cy="50%" r="27%" fill="white" />
 			</svg>
 			<svg style={STYLE.eye.right}>
+				<title>right eye</title>
 				<circle cx="50%" cy="50%" r="25%" fill="white" />
 			</svg>
 		</>
