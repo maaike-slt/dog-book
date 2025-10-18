1719396d7c95f768fae43cf2642e2b06b9e4faba
Alexdelia <alexandre.delille.57@gmail.com>
2025-10-18 19:50:57 +0200

    ci: simple `deploy` script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+DIST_DIR="dist"
+
+if [ ! -d "$DIST_DIR" ]; then
+	deno task build
+fi
+
+if [ ! -f "package.json" ]; then
+	grep '"homepage":' deno.jsonc | sed 's/.*: *"\(.*\)".*/{\n  "homepage": "\1"\n}/' > package.json
+fi
+
+deno run -A npm:gh-pages \
+	--dist="$DIST_DIR" \
+	--src='{index.html,*.png,assets/*.{js,css}}' \
+	--remove='{*,.*,.*/*,.*/**/*}' \
+	--branch=gh-pages \
+	--message='[skip ci] deploy gh-pages'
