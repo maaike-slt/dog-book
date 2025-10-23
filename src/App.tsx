029426f65d7e287cb5554209a12c337dea3f1ab6
maaike-slt <maaikesloot@home.nl>
2025-10-23 19:55:05 +0200

    feat: new `StatBtn`

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css"
 import DogVote from "./dog_vote/DogVote.tsx"
 import EyeFollow from "./eye_follow/EyeFollow.tsx"
 import MainTitle from "./main_title/MainTitle.tsx"
+import StatBtn from "./stat_btn/StatBtn.tsx"
 
 const App: React.FC = () => {
 	return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
 			<div className="main-container">
 				<MainTitle />
 				<DogVote />
+				<StatBtn />
 			</div>
 
 			<EyeFollow />
---
+++ src/stat_btn/StatBtn.tsx
@@ -0,0 +1,14 @@
+import type React from "react"
+import "./StatBtn.css"
+
+const StatBtn: React.FC = () => {
+	return (
+		<button
+			type="button"
+			className="stat-button"
+			// onClick={onClick}
+		/>
+	)
+}
+
+export default StatBtn
