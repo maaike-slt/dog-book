561cb300fa78f1fe3f514b4538299be600046b46
maaike-slt <maaikesloot@home.nl>
2025-10-25 15:30:44 +0200

    working button to stat page

--- src/stat_page/StatPage.tsx
+++ src/stat_page/StatPage.tsx
@@ -1,11 +1,17 @@
 import type React from "react"
+import { useState } from "react"
 import "./StatPage.css"
 
 const StatPage: React.FC = () => {
+	const [open, setOpen] = useState(false)
 	return (
-		<div className="stat-page">
+		<div className={`stat-page ${open ? " stat-page-open" : ""}`}>
 			ola muchachos
-			<button type="button" className="stat-button">
+			<button
+				type="button"
+				className="stat-button"
+				onClick={() => setOpen(true)}
+			>
 				stats
 			</button>
 		</div>
